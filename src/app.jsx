3027f41d0c81077fe537ea31e4a2219bbdbbed3f
Jonathan Franzone <jonathan@franzone.com>
2021-01-27 20:14:39 -0600

    Step 1 - Added the setup code from the React tutorial

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,71 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.css';
 
+class Square extends React.Component {
+    render() {
+        return (
+        <button className="square">
+            {/* TO-DO */}
+        </button>
+        );
+    }
+}
+  
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+        );
+    }
+}
+
+// ========================================
+  
 function render() {
-    ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+    ReactDOM.render(
+        <Game />,
+        document.getElementById('root')
+    );
 }
 
 render();
